7c74e19ad274fa0e42d6c9157fd15f2d8cdff52d
Guilherme Lucas da Silva <gulucasd@microsoft.com>
2020-09-17 15:25:52 -0300

    fix: fix number of problems

--- tests/inclusivelint_tests.sh
+++ tests/inclusivelint_tests.sh
@@ -24,7 +24,7 @@ function linter_should_find_contraventions() {
     actualErrors=0
     actualWarnings=0
     expectedErrors=4
-    expectedWarnings=3
+    expectedWarnings=5
 
     # act
     count_contraventions "./samples-1" $actualErrors $actualWarnings
